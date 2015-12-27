c05261181d24abb614580d1924a2839bca0f67a9
brunthavan <bruntha1991@hotmail.com>
2015-12-27 12:51:16 +0530

    modofication to implicit aspect percentage calculation

--- src/main/java/eatery/implicitAspect/PercentageCalculation.java
+++ src/main/java/eatery/implicitAspect/PercentageCalculation.java
@@ -13,13 +13,34 @@ import java.util.List;
  * Created by bruntha on 12/25/15.
  */
 public class PercentageCalculation {
-    List anns = new ArrayList();
+    List anns;
     int noOfSentence = 0;
     int noOf_1_Aspect = 0;
     int noOf_2_Aspect = 0;
     int noOf_3_Aspect = 0;
     int currentCount = 0;
     int lastAnnIndex = 0;
+    String[] annFile = {
+            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_14 Ann_Sorted.txt",
+            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_1 Ann_Sorted.txt",
+            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_5 Ann_Sorted.txt",
+            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_11 Ann_Sorted.txt",
+    };
+
+    //    String[] annFile = {"/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/ta.txt"};
+//    String[] reviewFile = {"/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/t.txt"};
+    String[] reviewFile = {
+            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_14.txt",
+            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_1.txt",
+            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_5.txt",
+            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_11.txt",
+    };
+
+    public static void main(String[] args) {
+        PercentageCalculation percentageCalculation = new PercentageCalculation();
+        percentageCalculation.calculate();
+//        percentageCalculation.toString();
+    }
 
     @Override
     public String toString() {
@@ -31,30 +52,14 @@ public class PercentageCalculation {
                 '}';
     }
 
-//    String[] annFile = {"/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/ta.txt"};
-//    String[] reviewFile = {"/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/t.txt"};
-
-    String[] annFile = {"/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_14 Ann_Sorted.txt",
-            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_1 Ann_Sorted.txt",
-            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_5 Ann_Sorted.txt",
-            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_11 Ann_Sorted.txt",
-    };
-    String[] reviewFile = {"/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_14.txt",
-            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_1.txt",
-            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_5.txt",
-            "/home/bruntha/Documents/Softwares/brat-v1.3_Crunchy_Frog/data/Eatery/Test/u_11.txt",
-    };
-
-    public static void main(String[] args) {
-        PercentageCalculation percentageCalculation = new PercentageCalculation();
-        percentageCalculation.calculate();
-        percentageCalculation.toString();
-    }
-
     public void calculate() {
         try {
             for (int i = 0; i < annFile.length; i++) {
+                currentCount = 0;
+                lastAnnIndex = 0;
+                anns = new ArrayList();
                 readAnnFile(annFile[i]);
+                System.out.println("ann size " + anns.size());
                 calculatePercentage(reviewFile[i]);
             }
 
@@ -63,9 +68,9 @@ public class PercentageCalculation {
             System.out.println("No of 2 aspect in one sentence " + noOf_2_Aspect);
             System.out.println("No of 3 aspect in one sentence " + noOf_3_Aspect);
 
-            System.out.println("Percentage of 1 implicit aspect occurrence sentences " +noOf_1_Aspect*100.0/noOfSentence*1.0+"%");
-            System.out.println("Percentage of 2 implicit aspect occurrence sentences " +noOf_2_Aspect*100.0/noOfSentence*1.0+"%");
-            System.out.println("Percentage of 3 implicit aspect occurrence sentences " +noOf_3_Aspect*100.0/noOfSentence*1.0+"%");
+            System.out.println("Percentage of 1 implicit aspect occurrence sentences " + noOf_1_Aspect * 100.0 / noOfSentence * 1.0 + "%");
+            System.out.println("Percentage of 2 implicit aspect occurrence sentences " + noOf_2_Aspect * 100.0 / noOfSentence * 1.0 + "%");
+            System.out.println("Percentage of 3 implicit aspect occurrence sentences " + noOf_3_Aspect * 100.0 / noOfSentence * 1.0 + "%");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -80,9 +85,10 @@ public class PercentageCalculation {
         while ((line = br.readLine()) != null) {
             String[] annotations = line.split("[ \t]");
             String word = line.substring(line.lastIndexOf('\t') + 1);
-//            if (annotations[1].startsWith("O")) {
-            anns.add(new Ann(Integer.parseInt(annotations[2]), Integer.parseInt(annotations[3]), annotations[1], word));
-//            }
+            if (!annotations[1].matches("Food") && !annotations[1].matches("F_FoodItem")) {
+                if (annotations[1].startsWith("P"))
+                    anns.add(new Ann(Integer.parseInt(annotations[2]), Integer.parseInt(annotations[3]), annotations[1], word));
+            }
 
         }
         br.close();
@@ -109,22 +115,18 @@ public class PercentageCalculation {
 
     public void updateAspectCount(String sentence, int countMax) {
         int countAspect = 0;
-        System.out.println(sentence);
+//        System.out.println(sentence);
         for (int i = lastAnnIndex; i < anns.size(); i++) {
             Ann ann = (Ann) anns.get(i);
             if (ann.getEndIndex() <= countMax) {
-                if (!ann.getApsect().toLowerCase().contains(ann.getWord().toLowerCase())) {
-//                    if (!ann.getApsect().equalsIgnoreCase(ann.getWord())) {
-                        System.out.println(ann.getApsect() + "\t" + ann.getWord());
-                        countAspect++;
-//                    }
-                }
+                System.out.println(ann.getApsect() + "\t" + ann.getWord());
+                countAspect++;
             } else {
                 lastAnnIndex = i;
                 break;
             }
         }
-
+//        System.out.println(countAspect);
         switch (countAspect) {
             case 1:
                 noOf_1_Aspect++;
@@ -137,5 +139,16 @@ public class PercentageCalculation {
                 break;
             default:
         }
+    }
+
+    public boolean isImplicit(Ann ann) {
+        if (ann.getApsect().equalsIgnoreCase(ann.getWord()))
+            return false;
+        else if (ann.getApsect().toLowerCase().contains(ann.getWord().toLowerCase()))
+            return false;
+        else if (ann.getApsect().matches("A_LocatedArea") && ann.getWord().matches("location"))
+            return false;
+        else
+            return true;
     }
 }
